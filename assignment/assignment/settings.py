a520dad09150908c2dd9fca6f814d8bda79bc9ef
tirthb101 <bhatiatirth01@gmail.com>
2025-05-29 12:26:45 +0530

    Update settings for production readiness and add Procfile for deployment

--- assignment/assignment/settings.py
+++ assignment/assignment/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-bx41waizf%@b#pc$(qm_ij_qs*g-fsq27*$%(fsf^g$k3+3f_4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
